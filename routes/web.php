1236f8e080d5967aa001edd542aa15c51096b490
nguyenhuan123 <huanngcd18611@fpt.edu.vn>
2023-03-26 23:49:47 +0700

    show home and detail product, product in categoy

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,21 @@ Route::group(['namespace' => 'Frontend'], function() {
     Route::get('','HomeController@index')->name('get.home'); // trang chủ
 	Route::get('/home', 'HomeController@index'); // trang chủ
 
-    
+    Route::get('ajax-load-product-recently','HomeController@getLoadProductRecently')->name('ajax_get.product_recently');
+    Route::get('ajax-load-product-by-category','HomeController@getLoadProductByCategory')->name('ajax_get.product_by_category');
+
+//    Route::get('ajax-load-slide','HomeController@loadSlideHome')->name('ajax_get.slide');
+    Route::get('san-pham','ProductController@index')->name('get.product.list'); // list sp
+    Route::get('dan-muc/{slug}','CategoryController@index')->name('get.category.list'); // list sp theo danh mục
+
+    Route::get('san-pham/{slug}','ProductDetailController@getProductDetail')->name('get.product.detail'); // chi tiet sp
+    Route::get('san-pham/{slug}/danh-gia','ProductDetailController@getListRatingProduct')->name('get.product.rating_list'); // list dánh giá của sp đó
+
+    Route::get('bai-viet','BlogController@index')->name('get.blog.home'); // bai viet
+    Route::get('menu/{slug}','BlogMenuController@getArticleByMenu')->name('get.article.by_menu'); // bai viết theo menu
+    Route::get('bai-viet/{slug}','ArticleDetailController@index')->name('get.blog.detail'); // chi tiet bai viet
+    // GIỏ hàng
+    Route::get('don-hang','ShoppingCartController@index')->name('get.shopping.list'); // gio hang
 
     Route::prefix('shopping')->group(function () {
         Route::get('add/{id}','ShoppingCartController@add')->name('get.shopping.add'); // thêm giỏ hàng
